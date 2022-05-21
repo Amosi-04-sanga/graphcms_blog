df69aa9e887b9c9848bba39be1e885f7d1ba41a2
amosi ally sanga <sangaamosi04@gmail.com>
2022-05-22 02:42:05 +0300

    css fixing

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -51,7 +51,7 @@ const PostDetails = ({ post }) => {
   };
 
   return (
-    <div className='pr-2'>
+    <div className=''>
       <div className={styles.featuredImage}>
         <img
           src={post.featuredImage.url}
