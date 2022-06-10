55e8386341cd096e31ef0aa6c53ed3495e023a0f
amosi ally sanga <sangaamosi04@gmail.com>
2022-06-10 05:13:59 +0300

    css refactory

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -33,7 +33,7 @@ const PostDetails = ({ post }) => {
           </div>
         )
       case 'paragraph':
-        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+        return <p key={index} className="mb-2">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
       case 'heading-four':
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
       case 'image':
