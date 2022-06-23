8e409002467767a7c57f4067fcbb79a6ed119b2d
amosi ally sanga <sangaamosi04@gmail.com>
2022-06-24 02:31:05 +0300

    working on graphcms rich text render

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,58 +1,10 @@
+import { RichText } from '@graphcms/rich-text-react-renderer'
 import moment from 'moment'
 import React from 'react'
 import styles from '../styles/Post.module.css'
 
+
 const PostDetails = ({ post }) => {
-
-  const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text;
-
-    if (obj) {
-      if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
-      }
-
-      if (obj.italic) {
-        modifiedText = (<i key={index}>{text}</i>);
-      }
-
-      if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
-      }
-    }
-
-    switch (type) {
-      case 'heading-three':
-        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-      case 'heading-two':
-        return <h2 key={index} className="text-xl my-4 text-center">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h2>;
-      case 'code-block':
-        return (
-          <div className="bg-orange-100 text-blue-900 rounded my-4 p-2 overflow-auto font-thin">
-            <pre key={index} className="text-sm font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</pre>
-          </div>
-        )
-      case 'paragraph':
-        return <p key={index} className="my-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-      case 'heading-six':
-        return <i key={index} className={`text-md block my-2 ${styles.notes}`}>{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</i>;
-      case 'image':
-        return (
-          <div data-role="imagemagnifier" data-magnifier-mode="glass" >
-            <img
-              key={index}
-              alt={obj.title}
-              height={obj.height}
-              width={"100%"}
-              src={obj.src}
-              className="postFrontPhoto"
-            />
-          </div>
-        );
-      default:
-        return modifiedText;
-    }
-  };
 
   return (
     <div className={styles.whitePaper} >
@@ -70,17 +22,58 @@ const PostDetails = ({ post }) => {
       <p className='text-center mb-4 mt-12 font-bold uppercase text-sm' > {post.title} </p>
       <div className="p-2">
         {
-          post.content.raw.children.map((typeObj, index) => {
-            const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item))
-
-            return getContentFragment(index, children, typeObj, typeObj.type)
-          })
+          console.log(post.content)
         }
+
+        < RichText
+          content={post.content.raw}
+          renderers={{
+            h6: ({ children }) => <h6 className={`text-md block my-2 ${styles.notes}`} >{children}</h6>,
+            h2: ({ children }) => <h2 className="text-center font-bold" >{children}</h2>,
+
+            a: ({ children, openInNewTab, href, rel, ...rest }) => {
+              if (href.match(/^https?:\/\/|^\/\//i)) {
+                return (
+                  <a
+                    href={href}
+                    className="text-red-800"
+                    target={openInNewTab ? '_blank' : '_self'}
+                    rel={rel || 'noopener noreferrer'}
+                    {...rest}
+                  >
+                    {children}
+                  </a>
+                );
+              }
+
+              return (
+                <Link href={href}>
+                  <a {...rest}>{children}</a>
+                </Link>
+              );
+            },
+
+            img: ({ src }) => <img className={styles.postImage} src={src} alt="" />,
+            code_block: ({ children }) => {
+              return (
+                <div className="relative">
+                  <div className="bg-orange-100 text-blue-900 rounded my-4 p-2 overflow-auto font-thin">
+                    <svg className='fill-blue-400 hover:fill-blue-600 transition duration-200 w-5 h-5 absolute right-2 top-2 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M384 96L384 0h-112c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48H464c26.51 0 48-21.49 48-48V128h-95.1C398.4 128 384 113.6 384 96zM416 0v96h96L416 0zM192 352V128h-144c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48h192c26.51 0 48-21.49 48-48L288 416h-32C220.7 416 192 387.3 192 352z" /></svg>
+                    <pre className="text-sm font-semibold mb-4">{children}</pre>
+                  </div>
+                </div>
+              )
+            },
+
+            p: ({ children }) => <p className="my-4" >{children}</p>,
+
+
+          }}
+        />
       </div>
     </div>
   )
 }
 
 export default PostDetails
-
 
