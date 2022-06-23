ae728235443ac1ecb3198d3c2c9c065436a37c19
amosi ally sanga <sangaamosi04@gmail.com>
2022-06-24 02:46:02 +0300

    bug fixing

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -38,7 +38,7 @@ const PostDetails = ({ post }) => {
                     href={href}
                     className="text-red-800"
                     target={openInNewTab ? '_blank' : '_self'}
-                    rel={rel || 'noreferrer'}
+                    rel='noopener'
                     {...rest}
                   >
                     {children}
